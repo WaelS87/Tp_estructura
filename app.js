af377e7eaf7a2c84ece0378cf8dcfe9df5e93287
WaelS87 <wfakhrji@gmail.com>
2022-06-22 16:37:18 -0300

    agrega articulos en html

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express=require('express');
 const app=express();
 const path =require('path');
 const puerto=3030;
+app.use(express.static('Public'))
 app.listen(puerto,()=>console.log('el servidor esta coriiendo en http://localhost:'+ puerto))
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
