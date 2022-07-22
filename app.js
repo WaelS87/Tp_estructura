b98307777fe53f3a0208d8620de4002ad40b3a0a
WaelS87 <wfakhrji@gmail.com>
2022-07-22 16:36:02 -0300

    cree la rama

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const app=express();
 const path =require('path');
 const puerto=3030;
 app.use(express.static('Public'))
-app.listen(puerto,()=>console.log('el servidor esta coriiendo en http://localhost:'+ puerto))
+
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+pp.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')))
+pp.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html')))
+
+
+
+
+app.listen(puerto,()=>console.log('el servidor esta coriiendo en http://localhost:'+ puerto))
